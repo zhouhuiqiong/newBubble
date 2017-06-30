74f0a01047ddfcb4bef49082adcfa861515886f3
zhouhuiqiong <zhq_189@163.com>
2017-06-30 17:16:16 +0800

    '修改'

--- src/js/public.js
+++ src/js/public.js
@@ -16,7 +16,7 @@
 			//跨域
 			$.ajax({  
 		        type: 'post',  
-		        url : 'http://118.178.188.7:8104/'+ data.url +'.action',  
+		        url : 'http://47.92.54.8/'+ data.url +'.action',
 		        data: data.data, 
 		        success: function(results){
 		        	if(results.state == 200){
